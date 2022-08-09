17174de52110fb5c186086abfe95b900143d8681
Igor Grankin <i.grankin@dodopizza.com>
2022-08-09 17:40:48 +0300

    Initial Commit

---
+++ string calc/string_calc.h
@@ -0,0 +1,18 @@
+//
+//  string_calc.h
+//  string calc
+//
+//  Created by Игорь Гранкин on 09.08.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for string_calc.
+FOUNDATION_EXPORT double string_calcVersionNumber;
+
+//! Project version string for string_calc.
+FOUNDATION_EXPORT const unsigned char string_calcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <string_calc/PublicHeader.h>
+
+
